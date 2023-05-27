71011509adcb06d5bc998180264927cc58c14766
juraxh <hrabovsky.jura@gmail.com>
2023-05-27 08:36:51 +0200

    improved examples in main.cc

--- main.cc
+++ main.cc
@@ -3,6 +3,7 @@
 //#include "re2/regexp.h"
 #include "re2/ca.hh"
 #include "re2/ca_builder.hh"
+#include "util/utf.h"
 
 
 using namespace std;
@@ -11,39 +12,46 @@ using namespace CA;
 void print_char_class(re2::CharClass *cc) {
     cout << "char class:\n";
     for (re2::RuneRange &range : *cc) {
-        cout << '\t' << to_string(range.lo) << " - " << to_string(range.hi) << endl;
+        char lo[5]; 
+        char hi[5]; 
+        lo[re2::runetochar(lo, &range.lo)] = '\0';
+        hi[re2::runetochar(hi, &range.hi)] = '\0';
+        cout << '\t' << to_string(range.lo) << ": " << lo
+            << " - " << to_string(range.hi) << ": " << hi << endl;
     }
 }
 
-int main() {
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        cerr << "missing pattern argument\n";
+        return 1;
+    }
     //auto b = Builder("[^©-®]");
-    auto b = Builder("aa|bb|cc");
+    auto b = Builder(argv[1]);
     auto regexp = b.regexp();
-    cout << regexp->ToString() << "\n";
-    regexp = remove_captures(regexp);
-    cout << regexp->ToString() << "\n";
-    cout << "is nullable: " << is_nullable(regexp) << "\n";
-    regexp = normalize(regexp);
-    cout << regexp->ToString() << "\n";
-    auto eq = Equation::get_eq(regexp);
-
-#if 0
-    auto b = Builder("[^a-b]");
-    auto regexp = b.regexp();
-    auto cc = regexp->cc(); 
-    print_char_class(cc);
-    auto negated_cc = cc->Negate();
-    print_char_class(negated_cc);
-
-    auto p = b.prog();
-    cout << "bytemap_range: " << b.bytemap_range() << endl;
-    cout << p->Dump();
+    cout << "original: " << regexp->ToString() << "\n";
     auto regexp_str = b.regexp()->ToString();
     cout << regexp_str << endl;
     for (char &a : regexp_str) {
         cout << a << ", ";
     }
+    auto p = b.prog();
+    cout << "bytemap_range: " << b.bytemap_range() << endl;
+    cout << p->Dump();
     cout << endl;
-#endif
+    regexp = remove_captures(regexp);
+    cout << "without captures: " << regexp->ToString() << "\n";
+    cout << "is nullable: " << is_nullable(regexp) << "\n";
+    regexp = normalize(regexp);
+    cout << "normalized: " << regexp->ToString() << "\n";
+    auto eq = Equation::get_eq(regexp);
+    
+    if (regexp->op() == re2::kRegexpCharClass) {
+        auto cc = regexp->cc(); 
+        print_char_class(cc);
+        auto negated_cc = cc->Negate();
+        cout << "negated: \n";
+        print_char_class(negated_cc);
+    }
 
 }
