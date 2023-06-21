7f58bb2987c6fe8ee86642c42dcdf6ecaa31800c
Jiri Hrabovsky <hrabovsky.jura@gmail.com>
2023-06-21 21:16:01 +0200

    different prog to show graph, for larger graphs

--- ca_graph.sh
+++ ca_graph.sh
@@ -1,5 +1,5 @@
-./build/ca_builder $1 >tmp_graph.dot
+./build/ca_builder "$1" >tmp_graph.dot
 dot -Tpng tmp_graph.dot -o tmp_graph.png
 rm tmp_graph.dot
-feh tmp_graph.png
+shotwell tmp_graph.png
 rm tmp_graph.png
